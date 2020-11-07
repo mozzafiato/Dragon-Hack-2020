e41c4a2ab751345a0b619a6d3a8ee8b36c46bbe3
Nina Velikajne <nv6920@student.uni-lj.si>
2020-11-07 23:54:49 +0100

    save to db

--- linearRegression/LinearRegression.py
+++ linearRegression/LinearRegression.py
@@ -6,7 +6,7 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 
 # Use a service account
-cred = credentials.Certificate('.\services.json')
+cred = credentials.Certificate(r'services.json')
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
